1561cf475076dc7765d25353476243238c77bd50
Danil <front.end.poznyakov@gmail.com>
2020-04-08 23:44:47 +0300

    added CRUD & migrations & models

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const fs        = require('fs');
 const path      = require('path');
 const Sequelize = require('sequelize');
 const env       = process.env.NODE_ENV || 'development';
-const config    = require(path.join(__dirname, 'config.js'))[env];
+const config    = require(path.join(__dirname, './config.js'))[env];
 let models      = {};
 const modelsFolderPath = './models';
 
@@ -24,4 +24,3 @@ Object.keys(models).forEach(modelName => {
 sequelize.models = models;
 
 module.exports = sequelize;
-
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = require('../db').models.user;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const result = await User.findAll();
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, err });
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const data = {
+      username,
+      email,
+      password
+    }
+
+    const result = await User.create(data);
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, err });
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+    const data = {
+      username,
+      email,
+      password
+    }
+
+    const result = await User.update(data, { where: { id } });
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, err });
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await User.destroy({ where: { id } });
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, err });
+  }
 });
 
 module.exports = router;
