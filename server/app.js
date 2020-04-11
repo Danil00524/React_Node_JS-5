549e5140af2f0f2814d0dee83ba1f044b0a8792b
Danil <front.end.poznyakov@gmail.com>
2020-04-11 12:33:48 +0300

    added authtorized on local strategy

--- server/app.js
+++ server/app.js
@@ -3,11 +3,58 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(
+  session({
+    store: new FileStore(),
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  // здесь необходимо найти пользователя с данным id в БД
+  // но он у нас один и мы просто сравниваем
+  const _user = user.id === id ? user : false;
+  done(null, _user);
+});
+
+const user = {
+  id: 1,
+  email: 'email@gmail.com',
+  password: '123',
+};
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email'
+    },
+    (email, password, done) => {
+      if (email === user.email && password === user.password) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    }
+  )
+)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,15 +67,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,63 +1,84 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 const User = require('../db').models.user;
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await User.findAll();
-    res.json({ success: true, data: result });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, err });
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) next(err);
+
+    if (!user) res.send('Укажите правильный email и пароль!');
+
+    req.login(user, err => {
+      return res.send('Вы удачно прошли аутентификацию!')
+    })
+  })(req, res, next);
+  // try {
+  //   const result = await User.findAll();
+  //   res.json({ success: true, data: result });
+  // } catch (err) {
+  //   console.error(err);
+  //   res.json({ success: false, err });
+  // }
+});
+
+router.get('/secret', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send('Вы ок.')
+  } else {
+    res.status(403).send('Все не ок.');
   }
 });
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-    const data = {
-      username,
-      email,
-      password
-    }
-
-    const result = await User.create(data);
-    res.json({ success: true, data: result });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, err });
-  }
+router.get('/signout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
-router.put('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { username, email, password } = req.body;
-    const data = {
-      username,
-      email,
-      password
-    }
+// router.post('/registration', async (req, res, next) => {
+//   try {
+//     const { username, surName, firstName, middleName, password } = req.body;
+//     const data = {
+//       username, surName, firstName, middleName, password
+//     }
 
-    const result = await User.update(data, { where: { id } });
-    res.json({ success: true, data: result });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, err });
-  }
-});
+//     const result = await User.create(data);
+//     res.json({ success: true, data: result });
+//   } catch (err) {
+//     console.error(err);
+//     res.json({ success: false, err });
+//   }
+// });
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
+// router.put('/:id', async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const { username, email, password } = req.body;
+//     const data = {
+//       username,
+//       email,
+//       password
+//     }
 
-    const result = await User.destroy({ where: { id } });
-    res.json({ success: true, data: result });
-  } catch (err) {
-    console.error(err);
-    res.json({ success: false, err });
-  }
-});
+//     const result = await User.update(data, { where: { id } });
+//     res.json({ success: true, data: result });
+//   } catch (err) {
+//     console.error(err);
+//     res.json({ success: false, err });
+//   }
+// });
+
+// router.delete('/:id', async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+
+//     const result = await User.destroy({ where: { id } });
+//     res.json({ success: true, data: result });
+//   } catch (err) {
+//     console.error(err);
+//     res.json({ success: false, err });
+//   }
+// });
 
 module.exports = router;
